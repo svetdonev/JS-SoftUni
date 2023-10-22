f9119242314bfeb6983297b5f6913fc2eb7682d7
Svetlozar Donev <svetdonev1@gmail.com>
2023-10-22 13:09:25 +0300

    Lab solved problems

---
+++ test/2-sum-numbers.test.js
@@ -0,0 +1,28 @@
+const sum = require('../unit-testing/2-sum-numbers.js');
+const expect = require('chai').expect;
+
+describe('Sum of numbers', () => {
+    it('Should return the sum of the numbers in the array', () => {
+        let numbers = [1, 2, 3, 4, 5];
+        let actualSum = sum(numbers);
+        let expectedSum = 15;
+
+        expect(actualSum).to.equal(expectedSum);
+    });
+
+    it('Should return the sum with negative number in the array', () => {
+        let numbers = [1, 2, 3, 4, -5];
+        let actualSum = sum(numbers);
+        let expectedSum = 5;
+
+        expect(actualSum).to.equal(expectedSum);
+    });
+
+    it('Should return zero when only zero is given', () => {
+        let numbers = [0, 0, 0];
+        let actualSum = sum(numbers);
+        let expectedSum = 0;
+
+        expect(actualSum).to.equal(expectedSum);
+    });
+});
