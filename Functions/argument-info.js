338b3b681197212b4f35e2436f9a50756eda6eac
Svetlozar Donev <svetdonev1@gmail.com>
2023-10-22 20:04:58 +0300

    Exercise solved problems

---
+++ Functions/argument-info.js
@@ -0,0 +1,19 @@
+function solve() {
+    let data = {};
+
+    Array.from(arguments).forEach((line) => {
+        let currentType = typeof line;
+        console.log(`${currentType}: ${line}`);
+
+        if(!data[currentType]) {
+            data[currentType] = 0;
+        }
+        data[currentType]++;
+    });
+
+    Object.keys(data)
+    .sort((a, b) => data[b] - data[a])
+    .forEach((key) => console.log(`${key} = ${data[key]}`));
+}
+
+solve('cat', 42, function() { console.log('Hello world!'); });
