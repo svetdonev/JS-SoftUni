0156898cd5172a6c234c3248930e9a4c249b3703
Svetlozar Donev <svetdonev1@gmail.com>
2023-11-02 00:02:55 +0200

    Update app.js

--- DOM/HellsKitchen/app.js
+++ DOM/HellsKitchen/app.js
@@ -19,14 +19,38 @@ function solve() {
                 
                 if(!output.hasOwnProperty(restaurantName)) {
                     output[restaurantName] = {};
-                } else {
+                }
+                if(output.hasOwnProperty(restaurantName)) {
                     output[restaurantName][name] = Number(salary);
                 }
 
             }
        }
-       console.log(output);
-    }
- }
+       let entries = Object.entries(output);
 
- //["Mikes - Steve 1000, Ivan 200, Paul 800", "Fleet - Maria 850, Janet 650"]
+       for (const entry of entries) {
+        //['PizzaHut', {...}]
+            let key = entry[0];
+            let values = Object.values(entry[1]);
+
+            for(let salary of values) {
+                totalSalary += salary;
+            }
+            avgSalary = totalSalary / values.length;
+            
+            if(avgSalary >= currentAvgSalary) {
+                currentAvgSalary = avgSalary;
+                bestName = key;
+                totalSalary = 0;
+            }
+       }
+       let result = Object.entries(output[bestName]).sort((a, b) => b[1] - a[1]);
+       let print = '';
+
+       result.forEach(w => print += `Name: ${w[0]} With Salary: ${w[1]} `);
+       document.querySelector('#bestRestaurant p').textContent = `Name: ${bestName} Average Salary: ${currentAvgSalary.toFixed(2)} Best Salary: ${result[0][1].toFixed(2)}`;
+       document.querySelector('#workers p').textContent = print;
+    }
+}
+
+ //["PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660"]
