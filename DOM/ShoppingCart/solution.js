ac31ac5d839030bca41fd75de0a507aee436ab50
Svetlozar Donev <svetdonev1@gmail.com>
2023-11-13 23:16:44 +0200

    Update solution.js

--- DOM/ShoppingCart/solution.js
+++ DOM/ShoppingCart/solution.js
@@ -1,22 +1,20 @@
- function solve() {
-    let addButton1 = document.getElementsByClassName('add-product')[0];
-    let addButton2 = document.getElementsByClassName('add-product')[1];
-    let addButton3 = document.getElementsByClassName('add-product')[2];
+function solve() {
+    let addButton = document.getElementsByClassName('add-product');
+    let arrayAddButton = Array.from(addButton);
     let textArea = document.getElementsByTagName('textarea')[0];
     let price = 0;
     let outputProducts = [];
 
-    addButton1.addEventListener('click', addToCart);
-    addButton2.addEventListener('click', addToCart);
-    addButton3.addEventListener('click', addToCart);
-    
+    for (let i = 0; i < arrayAddButton.length; i++) {
+        arrayAddButton[i].addEventListener('click', addToCart)
+    }
 
     let checkoutBtn = document.getElementsByClassName('checkout')[0];
-
     checkoutBtn.addEventListener('click', checkOut);
 
     function checkOut() {
-        textArea.value += `You bought ${outputProducts.join(', ')} for ${price.toFixed(2)}.`;
+        let finalSting = `You bought ${outputProducts.join(', ')} for ${price.toFixed(2)}.`;
+        textArea.value += finalSting;
 
         let allButtons = document.getElementsByTagName('button');
 
@@ -28,17 +26,14 @@
     function addToCart(ev) {
         let productElement = ev.target.closest('.product');
         let productName = productElement.querySelector('.product-title').textContent;
-        parseFloat(productElement.querySelector('.product-line-price').textContent);
-
-        if (isNaN(productPrice)) {
-            return;
-        }
+        let productPrice = parseFloat(productElement.querySelector('.product-line-price').textContent);
 
         if (!outputProducts.includes(productName)) {
             outputProducts.push(productName);
-            price += Number(productPrice);
-
-            textArea.value += `Added ${productName} for ${productPrice.toFixed(2)} to the cart. \n`;
         }
+
+        let result = `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+        price += Number(productPrice);
+        textArea.value += result;
     }
 } 
