f51264739995fc12407205e5439900745676fbfd
Svetlozar Donev <svetdonev1@gmail.com>
2023-11-21 01:18:36 +0200

    Exam solved problems

--- Exam-1/Camping/app.js
+++ Exam-1/Camping/app.js
@@ -7,7 +7,7 @@ class SummerCamp {
             "child": 150,
             "student": 300,
             "collegian": 500
-        }
+        };
         this.listOfParticipants = [];
     }
 
@@ -17,27 +17,26 @@ class SummerCamp {
         } else {
             if (this.listOfParticipants.some(n => n.name === name)) {
                 return `The ${name} is already registered at the camp.`
-            } else {
-                if (money < this.priceForTheCamp[condition]) {
-                    return `The money is not enough to pay the stay at the camp.`;
-                }
-
-                let current = {
-                    name,
-                    condition,
-                    power: 100,
-                    wins: 0
-                };
-
-                this.listOfParticipants.push(current);
-                return `The ${name} was successfully registered.`;
             }
+
+            if (money < this.priceForTheCamp[condition]) {
+                return `The money is not enough to pay the stay at the camp.`;
+            }
+
+            let participant = {
+                name,
+                condition,
+                power: 100,
+                wins: 0
+            };
+
+            this.listOfParticipants.push(participant);
+            return `The ${name} was successfully registered.`;
         }
     }
 
     unregisterParticipant(name) {
         let initialLength = this.listOfParticipants.length;
-
         this.listOfParticipants = this.listOfParticipants.filter(n => n.name !== name);
 
         if (initialLength === this.listOfParticipants.length) {
@@ -49,44 +48,51 @@ class SummerCamp {
 
     timeToPlay(typeOfGame, participant1, participant2) {
         let p1 = this.listOfParticipants.find(x => x.name == participant1);
-        let p2 = this.listOfParticipants.find(x => x.name == participant2);
 
         if (typeOfGame == 'WaterBalloonFights') {
+            let p2 = this.listOfParticipants.find(x => x.name == participant2);
+
             if (p1 == undefined || p2 == undefined) {
                 return `Invalid entered name/s.`;
             }
 
-            let p1Condition = p1.condition;
-            let p2Condition = p2.condition;
-
-            if (p1Condition != p2Condition) {
-                throw Error (`Choose players with equal condition.`);
+            if (p1.condition != p2.condition) {
+                throw Error(`Choose players with equal condition.`);
             }
 
-            if(p1.power > p2.power) {
+            if (p1.power > p2.power) {
                 p1.wins++;
                 return `The ${participant1} is winner in the game ${typeOfGame}.`;
-            } else if(p1.power < p2.power){
+            } else if (p1.power < p2.power) {
                 p2.wins++;
                 return `The ${participant2} is winner in the game ${typeOfGame}.`;
             } else {
                 return `There is no winner.`;
             }
-        } else if(typeOfGame == 'Battleship') {
+        } else if (typeOfGame == 'Battleship') {
+            if (p1 == undefined) {
+                return `Invalid entered name/s.`;
+            }
             p1.power += 20;
             return `The ${participant1} successfully completed the game ${typeOfGame}.`;
         }
     }
 
     toString() {
-        let res = `${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}\n`;
-        //console.log(`${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}`);
-
-        for (const participant of this.listOfParticipants.sort((a, b) => b.wins- a.wins)) {
+        //let res = `${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}\n`;
+        /* for (const participant of this.listOfParticipants.sort((a, b) => b.wins - a.wins)) {
             res += `${participant.name} - ${participant.condition} - ${participant.power} - ${participant.wins}\n`;
-        }
+        } */
 
-        return res;
+        let result = [];
+        result.push(`${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}`);
+
+        this.listOfParticipants.sort((a, b) => b.wins - a.wins)
+            .forEach(x => {
+                result.push(`${x.name} - ${x.condition} - ${x.power} - ${x.wins}`);
+            });
+
+        return result.join('\n');
     }
 }
 
