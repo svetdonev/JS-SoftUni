5dac8cb6b20bcf36352da69e2bf54f1a3cf6dbc4
Svetlozar Donev <svetdonev1@gmail.com>
2023-11-21 00:20:52 +0200

    Exam solved problems

--- Exam-1/Camping/app.js
+++ Exam-1/Camping/app.js
@@ -2,23 +2,23 @@ class SummerCamp {
     constructor(organizer, location) {
         this.organizer = organizer;
         this.location = location;
-        this.priceForTheCamp = 
+        this.priceForTheCamp =
         {
-            "child": 150, 
-            "student": 300, 
+            "child": 150,
+            "student": 300,
             "collegian": 500
         }
         this.listOfParticipants = [];
     }
 
-    registerParticipant (name, condition, money) {
+    registerParticipant(name, condition, money) {
         if (!this.priceForTheCamp.hasOwnProperty(condition)) {
             throw Error('Unsuccessful registration at the camp.');
         } else {
-            if(this.listOfParticipants.find(n => this.name)) {
+            if (this.listOfParticipants.some(n => n.name === name)) {
                 return `The ${name} is already registered at the camp.`
             } else {
-                if(money < this.priceForTheCamp[condition]) {
+                if (money < this.priceForTheCamp[condition]) {
                     return `The money is not enough to pay the stay at the camp.`;
                 }
 
@@ -35,34 +35,67 @@ class SummerCamp {
         }
     }
 
-    unregisterParticipant (name) {
+    unregisterParticipant(name) {
         let initialLength = this.listOfParticipants.length;
 
         this.listOfParticipants = this.listOfParticipants.filter(n => n.name !== name);
 
         if (initialLength === this.listOfParticipants.length) {
-            throw Error(`The ${name} is not registered in the camp`);
+            throw Error(`The ${name} is not registered in the camp.`);
         }
 
         return `The ${name} removed successfully.`;
     }
 
-    timeToPlay (typeOfGame, participant1, participant2) {
-        if (typeOfGame == 'WaterBaloonFights') {
-            
+    timeToPlay(typeOfGame, participant1, participant2) {
+        let p1 = this.listOfParticipants.find(x => x.name == participant1);
+        let p2 = this.listOfParticipants.find(x => x.name == participant2);
 
-            return person1;
-            
-        } else if (typeOfGame == 'Battleship') {
+        if (typeOfGame == 'WaterBalloonFights') {
+            if (p1 == undefined || p2 == undefined) {
+                return `Invalid entered name/s.`;
+            }
 
+            let p1Condition = p1.condition;
+            let p2Condition = p2.condition;
+
+            if (p1Condition != p2Condition) {
+                throw Error (`Choose players with equal condition.`);
+            }
+
+            if(p1.power > p2.power) {
+                p1.wins++;
+                return `The ${participant1} is winner in the game ${typeOfGame}.`;
+            } else if(p1.power < p2.power){
+                p2.wins++;
+                return `The ${participant2} is winner in the game ${typeOfGame}.`;
+            } else {
+                return `There is no winner.`;
+            }
+        } else if(typeOfGame == 'Battleship') {
+            p1.power += 20;
+            return `The ${participant1} successfully completed the game ${typeOfGame}.`;
         }
+    }
+
+    toString() {
+        let res = `${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}\n`;
+        //console.log(`${this.organizer} will take ${this.listOfParticipants.length} participants on camping to ${this.location}`);
+
+        for (const participant of this.listOfParticipants.sort((a, b) => b.wins- a.wins)) {
+            res += `${participant.name} - ${participant.condition} - ${participant.power} - ${participant.wins}\n`;
+        }
+
+        return res;
     }
 }
 
 const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
-console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Petar Petarson", "child", 300));
 //console.log(summerCamp.timeToPlay("Battleship", "Petar Petarson"));
 console.log(summerCamp.registerParticipant("Sara Dickinson", "child", 200));
+console.log(summerCamp.timeToPlay("Battleship", "Sara Dickinson"));
 //console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Sara Dickinson"));
-console.log(summerCamp.registerParticipant("Dimitur Kostov", "student", 300));
-console.log(summerCamp.timeToPlay("WaterBalloonFights", "Petar Petarson", "Dimitur Kostov"));
+//console.log(summerCamp.registerParticipant("Dimitur Kostov", "student", 300));
+console.log(summerCamp.timeToPlay("WaterBalloonFights", "Sara Dickinson", "Petar Petarson"));
+console.log(summerCamp.toString());
