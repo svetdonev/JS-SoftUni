1c87d2f792b2e8ec5e8310791b12f5e5c19df6af
Svetlozar Donev <svetdonev1@gmail.com>
2023-11-24 17:13:31 +0200

    Exam solved problems

--- Exam-2/ArtGallery/app.js
+++ Exam-2/ArtGallery/app.js
@@ -32,7 +32,7 @@ class ArtGallery {
         let isExisting = this.guests.some(x => x.guestName === guestName);
 
         if (isExisting) {
-            return `${guestName} has already been invited.`;
+            throw new Error(`${guestName} has already been invited.`);
         }
 
         let points = 50;
@@ -47,29 +47,49 @@ class ArtGallery {
         return `You have successfully invited ${guestName}!`;
     }
 
-    buyArticle (articleModel, articleName, guestName) {
+    buyArticle(articleModel, articleName, guestName) {
         let existingArticle = this.listOfArticles.find(x => x.articleName === articleName && x.articleModel === articleModel);
 
-        if(!existingArticle) {
-            return `This article is not found.`;
+        if (!existingArticle) {
+            throw new Error(`This article is not found.`);
         }
 
-        if(existingArticle.quantity == 0) {
-            return `The {articleName} is not available.`;
+        if (existingArticle.quantity == 0) {
+            return `The ${articleName} is not available.`;
         }
 
         let existingGuest = this.guests.find(x => x.guestName === guestName);
-        if(!existingGuest) {
+        if (!existingGuest) {
             return `This guest is not invited.`;
         }
 
-        if(existingGuest.points - this.possibleArticles[articleModel] < 0) {
+        if (existingGuest.points - this.possibleArticles[articleModel] < 0) {
             return `You need to more points to purchase the article.`;
         } else {
             existingGuest.points -= this.possibleArticles[articleModel];
             existingGuest.purchaseArticle++;
             return `${guestName} successfully purchased the article worth ${this.possibleArticles[articleModel]} points.`
         }
+    }
+
+     showGalleryInfo(criteria) {
+        let result = [];
+
+        if(criteria == 'article') {
+            result.push(`Article information:`);
+
+            this.listOfArticles.forEach(x => {
+                result.push(`${x.articleModel} - ${x.articleName} - ${x.quantity}`);
+            });
+        } else if(criteria == 'guest') {
+            result.push('Guests information:');
+
+            this.guests.forEach(x => {
+                result.push(`${x.guestName} - ${x.purchaseArticle}`);
+            });
+        }
+
+        return result.join('\n');
     }
 }
 
@@ -79,6 +99,7 @@ artGallery.addArticle('Item', 'Ancient vase', 2);
 artGallery.addArticle('picture', 'Mona Liza', 1);
 artGallery.inviteGuest('John', 'Vip');
 artGallery.inviteGuest('Peter', 'Middle');
-console.log(artGallery.buyArticle('picture', 'Mona Liza', 'John'));
-console.log(artGallery.buyArticle('item', 'Ancient vase', 'Peter'));
-console.log(artGallery.buyArticle('item', 'Mona Liza', 'John'));
+artGallery.buyArticle('picture', 'Mona Liza', 'John');
+artGallery.buyArticle('item', 'Ancient vase', 'Peter');
+console.log(artGallery.showGalleryInfo('article'));
+console.log(artGallery.showGalleryInfo('guest'));
