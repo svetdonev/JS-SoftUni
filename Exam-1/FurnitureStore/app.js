cab384f076222e05cb9b24493b05677b6daca4c8
Svetlozar Donev <svetdonev1@gmail.com>
2023-11-28 01:16:49 +0200

    Exam solved problems

--- Exam-1/FurnitureStore/app.js
+++ Exam-1/FurnitureStore/app.js
@@ -1,62 +1,74 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    let modelElement = document.getElementById('model');
-    let yearElement = document.getElementById('year');
-    let descriptionElement = document.getElementById('description');
-    let priceElement = document.getElementById('price');
-    let addBtn = document.getElementById('add');
+    let modelInputElement = document.getElementById('model');
+    let yearInputElement = document.getElementById('year');
+    let descriptionInputElement = document.getElementById('description');
+    let priceInputElement = document.getElementById('price');
     let table = document.getElementById('furniture-list');
-    let moreInfoBtn = document.createElement('button');
-    let buyBtn = document.createElement('button');
+    let addBtn = document.getElementById('add');
 
     addBtn.addEventListener('click', (e) => {
         e.preventDefault();
+        let model = modelInputElement.value;
+        let description = descriptionInputElement.value;
+        let year = Number(yearInputElement.value);
+        let price = Number(priceInputElement.value);
 
-        if (modelElement.value != '' && descriptionElement.value != '' && yearElement.value > 0 && priceElement.value > 0) {
-            let trElement = document.createElement('tr');
-            trElement.classList.add('info');
+        if (modelInputElement.value && descriptionInputElement.value && price > 0 && year > 0) {
+            let rowElement = document.createElement('tr');
+            let modelTdElement = document.createElement('td');
+            let priceTdElement = document.createElement('td');
+            let actionBtns = document.createElement('td');
+            let moreInfoBtn = document.createElement('button');
+            let buyBtn = document.createElement('button');
+            let infoRowElement = document.createElement('tr');
 
-            let tdModelElement = document.createElement('td');
-            let tdPriceElement = document.createElement('td');
-            let tdBtnsElement = document.createElement('td');
-
+            rowElement.classList.add('info');
             moreInfoBtn.classList.add('moreBtn');
-            moreInfoBtn.innerText = 'More information';
             buyBtn.classList.add('buyBtn');
-            buyBtn.innerText = 'Buy it';
 
-            tdBtnsElement.appendChild(moreInfoBtn);
-            tdBtnsElement.appendChild(buyBtn);
+            moreInfoBtn.addEventListener('click', (e) => {
+                if (e.currentTarget.textContent == 'More Info') {
+                    e.currentTarget.textContent = 'Less info';
+                    infoRowElement.style.display = 'contents';
+                    let yearTdEl = document.createElement('td');
+                    let descriptionTdEl = document.createElement('td');
+                    descriptionTdEl.setAttribute('colspan', 3);
 
-            tdModelElement.textContent = modelElement.value;
-            tdPriceElement.textContent = Number(priceElement.value).toFixed(2);
+                    yearTdEl.textContent = `Year: ${year}`;
+                    descriptionTdEl.textContent = `Description: ${description}`;
 
-            trElement.appendChild(tdModelElement);
-            trElement.appendChild(tdPriceElement);
-            trElement.appendChild(tdBtnsElement);
-            table.appendChild(trElement);
+                    infoRowElement.appendChild(yearTdEl);
+                    infoRowElement.appendChild(descriptionTdEl);
+                } else {
+                    e.currentTarget.textContent = 'More Info';
+                    infoRowElement.style.display = 'none';
+                }
+            });
 
-            modelElement.value = '';
-            priceElement.value = '';
-            descriptionElement.value = '';
-            yearElement.value = '';
+            modelTdElement.textContent = model;
+            priceTdElement.textContent = price.toFixed(2);
+            moreInfoBtn.textContent = 'More Info';
+            buyBtn.textContent = 'Buy it';
+
+            actionBtns.appendChild(moreInfoBtn);
+            actionBtns.appendChild(buyBtn);
+            rowElement.appendChild(modelTdElement);
+            rowElement.appendChild(priceTdElement);
+            rowElement.appendChild(actionBtns);
+
+            let yearTdElement = document.createElement('td');
+            let descriptionTdElement = document.createElement('td');
+
+            infoRowElement.classList.add('hide');
+            infoRowElement.style.display = 'none';
+            yearTdElement.textContent = year;
+            descriptionTdElement.textContent = description;
+
+            table.appendChild(rowElement);
+            table.appendChild(infoRowElement);
         }
-    });
 
-    moreInfoBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        moreInfoBtn.innerText = 'Less Info';
-
-        let trElement = document.createElement('tr');
-        let tdPriceElement = document.createElement('td');
-        let tdDescriptionElement = document.createElement('td');
-        trElement.classList.add('hide');
-        trElement.style.display = 'contents';
-
-        trElement.classList.add('none');
-        trElement.appendChild(tdPriceElement);
-        trElement.appendChild(tdDescriptionElement);
-        table.appendChild(trElement);
     });
 }
